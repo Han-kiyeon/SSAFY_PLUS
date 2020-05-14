8f467fcf3d5a92a6752153e09066b291367ee175
Han-kiyeon <kylin3100@naver.com>
2020-05-15 08:51:04 +0900

    20200515 | Swagger UI 적용 | 한기연

--- backend/src/main/java/com/ssafy/springboot/web/PostsApiController.java
+++ backend/src/main/java/com/ssafy/springboot/web/PostsApiController.java
@@ -7,6 +7,8 @@ import com.ssafy.springboot.web.dto.PostsResponseDto;
 import com.ssafy.springboot.web.dto.PostsSaveRequestDto;
 import com.ssafy.springboot.web.dto.PostsUpdateRequestDto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
@@ -14,33 +16,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@Api(tags = {"1. Post"})
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
+    @ApiOperation(value="게시글 작성", notes="게시글을 작성한다.")
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
+    @ApiOperation(value="게시글 수정", notes="해당 id의 게시글을 수정한다.")
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
+    @ApiOperation(value="게시글 삭제", notes="해당 id의 게시글을 삭제한다.")
     @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
     }
 
+    @ApiOperation(value="게시글 조회", notes="해당 id의 게시글을 조회한다.")
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
+    @ApiOperation(value="모든 게시글 조회", notes="모든 게시글을 조회한다(id 역순)")
     @GetMapping("/api/v1/posts/list")
     public List<PostsListResponseDto> findAll() {
         return postsService.findAllDesc();
