3057c939dbbe9e73b84b74a3834ea3621e4a0334
Han-kiyeon <kylin3100@naver.com>
2020-05-26 17:55:49 +0900

    20200526 | 게시판 인증 추가 | 한기연

--- backend/src/main/java/com/ssafy/springboot/domain/board/BoardParty.java
+++ backend/src/main/java/com/ssafy/springboot/domain/board/BoardParty.java
@@ -1,4 +1,4 @@
-package com.ssafy.springboot.domain.post;
+package com.ssafy.springboot.domain.board;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -14,26 +14,26 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor //기본 생성자 자동추가
 @Entity //테이블과 링크될 클래스임을 나타냄(카멜케이스->언더스코어네이밍)
-public class PostLike {
+public class BoardParty {
 
     @Id //해당 테이블의 PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK 생성규칙: auto_increment
-    private Long pl_id;
+    private Long bp_id;
 
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "user_id")
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private User userlike;
+    private User user;
 
     @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "post_id")
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private Posts postlike;
+    private Board board;
 
     @Builder
-    public PostLike(User user,Posts post){
-        this.userlike=user;
-        this.postlike=post;
+    public BoardParty(User user, Board board) {
+        this.user = user;
+        this.board = board;
     }
 
 }
--- backend/src/main/java/com/ssafy/springboot/web/BoardController.java
+++ backend/src/main/java/com/ssafy/springboot/web/BoardController.java
@@ -1,7 +1,9 @@
 package com.ssafy.springboot.web;
 
+import com.ssafy.springboot.service.BoardPartyService;
 import com.ssafy.springboot.service.BoardService;
 import com.ssafy.springboot.web.dto.board.BoardListResponseDto;
+import com.ssafy.springboot.web.dto.board.BoardPartySaveRequestDto;
 import com.ssafy.springboot.web.dto.board.BoardSaveRequestDto;
 import com.ssafy.springboot.web.dto.board.BoardUpdateRequestDto;
 import io.swagger.annotations.Api;
@@ -19,6 +21,7 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
+    private final BoardPartyService boardPartyService;
 
     @ApiOperation(value = "게시판 생성")
     @PostMapping("") // 게시글 업로드
@@ -35,14 +38,21 @@ public class BoardController {
 
     @ApiOperation(value = "게시판 수정")
     @PutMapping("{board_id}") // 게시글 수정
-    public void update(@PathVariable Long bid, @RequestBody BoardUpdateRequestDto requestDto) {
-        boardService.update(bid, requestDto);
+    public void update(@PathVariable Long board_id, @RequestBody BoardUpdateRequestDto requestDto) {
+        boardService.update(board_id, requestDto);
     }
 
     @ApiOperation(value = "게시판 삭제")
     @DeleteMapping("{board_id}")
-    public void delete(@PathVariable Long bid) {
-        boardService.delete(bid);
+    public void delete(@PathVariable Long board_id) {
+        boardService.delete(board_id);
+    }
+
+
+    @ApiOperation(value = "게시판 가입")
+    @PostMapping("/join/{board_id}")
+    public void delete(@PathVariable Long board_id, @RequestBody BoardPartySaveRequestDto requestDto) {
+        boardPartyService.update(board_id, requestDto);
     }
 
 }
