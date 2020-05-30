77bd1aebd9fa5791d67395b185be93137e0785d5
Eumyounghyun <opwer032@naver.com>
2020-05-30 16:21:24 +0900

    20200530 | episode init | 음영현

--- frontend/src/components/Episode/EpisodeContainer.tsx
+++ frontend/src/components/Episode/EpisodeContainer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import EpisodePresenter from "./EpisodePresenter";
 
 export default class extends React.Component {
   componentDidMount() {
     window.scrollTo(0, 0);
   }
   render() {
-    return <div>Episode</div>;
+    return <EpisodePresenter />;
   }
 }
---
+++ frontend/src/components/Episode/EpisodePresenter.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface PortfolioIState {}
+
+function PortfolioPresenter({}: PortfolioIState) {
+  return <>Episodes</>;
+}
+
+export default PortfolioPresenter;
