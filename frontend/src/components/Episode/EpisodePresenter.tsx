d78f66564253678255904f25ac6fb5fd17de3700
Eumyounghyun <opwer032@naver.com>
2020-05-31 00:52:25 +0900

    20200531 | 음영현 | episode 타입 설정

--- frontend/src/components/Episode/EpisodePresenter.tsx
+++ frontend/src/components/Episode/EpisodePresenter.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-interface PortfolioIState {}
+interface PortfolioIState {
+  episodes: Array<{
+    id: number;
+    user_id: number;
+    date: string;
+    title: string;
+    strength: Array<string>;
+    content: string;
+  }>;
+}
 
-function PortfolioPresenter({}: PortfolioIState) {
-  return <>Episodes</>;
+function PortfolioPresenter({ episodes }: PortfolioIState) {
+  return (
+    <>
+      {episodes.map(episode => (
+        <div key={episode.id}>{episode.title}</div>
+      ))}
+    </>
+  );
 }
 
 export default PortfolioPresenter;
